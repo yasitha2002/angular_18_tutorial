d58d6c7ffaaa5e8c323a0bdc1664f435915ee894
Yasitha <yasithavas@gmail.com>
2025-12-11 23:32:19 -0800

    2nd commit

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RolesComponent } from '../../components/roles/roles.component';
+import { DesignationComponent } from '../../components/designation/designation.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [RolesComponent,DesignationComponent,CommonModule],
   templateUrl: './master.component.html',
   styleUrl: './master.component.css'
 })
 export class MasterComponent {
+      currentcomponent:String='';
 
+      changeTab(tabName:String){
+        this.currentcomponent= tabName;
+      }
 }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,35 +1,53 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component ,inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {IRole} from '../../model/interface/role' ;
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
 
   //Datatypes in typescript 
   // string,number,object, Date,Boolean,array,null,undefined
-  firstName:String="Angular Tutorial" ;
+  // firstName:String="Angular Tutorial" ;
 
-  AngularVersion = "Angular 18" ;
+  // AngularVersion = "Angular 18" ;
 
-  isActive:Boolean = false ;
+  // isActive:Boolean = false ;
 
-  currenDate : Date = new Date();
+  // currenDate : Date = new Date();
 
-  course_status:String = "Average" ;
+  // course_status:String = "Average" ;
 
-  inputType:String = "button" ;
+  // inputType:String = "button" ;
 
-  selectStatus:String = ""
+  // selectStatus:String = ""
 
 
   //Functions in typescript
-  ShowWelcomeAlert (){
-   alert("Welcome to Angular 18 Tutorial")
-  }
+  // ShowWelcomeAlert (){
+  //  alert("Welcome to Angular 18 Tutorial")
+  // }
+   
+   roleList: IRole [] = [];
+   http = inject(HttpClient);    //To call the API ,HttpClient should be injected.
 
+   ngOnInit() : void{        //ngOnInit is a lifecycle hook in Angular that runs after the component is created
+      this.getAllRoles()     //To use it, your component must implement the OnInit interface.
+   }
+   
+   getAllRoles (){       //As we are going to fetch data from the API it is a get.
+     this.http.get("/api/ClientStrive/GetAllRoles").subscribe((res:any)=>{
+     this.roleList = res.data;
+     })
+              //.subscribe() receives the response . Those response data gets received to the parameter 'res' which is of 'any' datatype    }        
 }
+}
+
